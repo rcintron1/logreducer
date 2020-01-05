11aae0fdf695a4d4e610a740ca86a89c5dafcd49
Rolando.Cintron <rolando.cintron@gmail.com>
2020-01-04 23:47:36 -0500

    feat:still working on tests

--- test/test_prompt.js
+++ test/test_prompt.js
@@ -1,6 +1,6 @@
 var assert = require('assert');
 const { exec } = require('child_process');
-noOptionExpectedResult='Options:\n  --help, -h     Show help                                             [boolean]\n  --version, -V  Show version number                                   [boolean]\n  --file, -f     <filename> Input file name of log                    [required]\n  --regex, -r    regex to parse through the logs\n  --output, -o   <filename output file name\n\nMissing required argument: file\n'
+noOptionExpectedResult='Options:\n  --help, -h     Show help                                             [boolean]\n  --version, -V  Show version number                                   [boolean]\n  --file, -f     <filename> Input file name of log                    [required]\n  --regex, -r    regex to parse through the logs\n  --output, -o   <filename> output file name\n\nMissing required argument: file\n'
 
 
 
