5bfee8b79b08424b40bca5a6fedea7014a151a66
Rolando.Cintron <rolando.cintron@gmail.com>
2020-01-07 07:38:07 -0500

    feat:adding more cli test with chai

--- test/test.js
+++ test/test.js
@@ -1,20 +1,27 @@
 var assert = require('assert');
+var expect = require('chai').expect
 const { exec } = require('child_process');
 const lr = new (require("../logreader"))({file:"./test/kafkaServer.out",logging:"mocha"})
 noOptionExpectedResult='Options:\n  --help, -h     Show help                                             [boolean]\n  --version, -V  Show version number                                   [boolean]\n  --file, -f     <filename> Input file name of log                    [required]\n  --regex, -r    regex to parse through the logs\n  --output, -o   <filename> output file name\n\nMissing required argument: file\n'
 
 
-describe('Test Login Prompt', function() {
+describe('Test CLI Prompt', function() {
   it('should return instructions when no option is included', function(done) {
     exec('node index.js', (error, stdout, stderr) => {
         assert.equal(`${stderr}`,noOptionExpectedResult);
         done()
     });
   });
+  it('should return count value of last record', function(done) {
+    exec('node index.js -f ./test/kafkaServer.out', (error, stdout, stderr) => {
+        // console.log(`${stdout}`)
+        expect(`${stdout}`).to.include(`count: 13`);
+        done()
+    });
+  });
 });
 
 describe('Test logreader function',function(){
-    
   it('should return an array',function(done){
     lr.readFile().then(data=>{
         assert(Array.isArray(data))
